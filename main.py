ec8da22386c258c19fd7dec8a8a9f25d593edc5b
Gaoxiang <82297961+letian-debug@users.noreply.github.com>
2025-11-25 20:47:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 # 导入各组成员模型
 from models.svm_regression import run_svr
 from models.knn_regression import run_knn
-# from models.ann_regression import run_ann
+from models.ann_regression import run_ann
 # from models.linear_regression import run_linear
 from models.rf_regression import run_rf
 
@@ -28,8 +28,8 @@ def main():
 
     print('\n--- KNN Regression ---')
     knn_metrics = run_knn(X_train_scaled, X_test_scaled, y_train, y_test)
-    # print('\n--- ANN Regression ---')
-    # ann_metrics = run_ann(X_train_scaled, X_test_scaled, y_train, y_test)
+    print('\n--- ANN Regression ---')
+    ann_metrics = run_ann(X_train_scaled, X_test_scaled, y_train, y_test)
     # print('\n--- Linear Regression ---')
     # linear_metrics = run_linear(X_train_scaled, X_test_scaled, y_train, y_test)
     print('\n--- Random Forest Regression ---')
@@ -41,7 +41,7 @@ def main():
     print(f"| SVM   | {svr_metrics['mse']:.4f} | {svr_metrics['rmse']:.4f} | {svr_metrics['r2']:.4f} |")
     print(f"| KNN   | {knn_metrics['mse']:.4f} | {knn_metrics['rmse']:.4f} | {knn_metrics['r2']:.4f} |")
     print(f"|  RF   | {rf_metrics['mse']:.4f} | {rf_metrics['rmse']:.4f} | {rf_metrics['r2']:.4f} |")
-
+    print(f"| ANN   | {ann_metrics['mse']:.4f} | {ann_metrics['rmse']:.4f} | {ann_metrics['r2']:.4f} |")
 
 if __name__ == "__main__":
     main()
